548285a413fc50139991583b33bbc2fc8eecaa83
Luis María Pizarro <lmpizarro@gmail.com>
2018-05-07 16:44:14 -0300

    np bin repre

--- src/pulse/pulse.py
+++ src/pulse/pulse.py
@@ -1,14 +1,28 @@
 import random
 import math 
 
+import numpy as np
+
 # S: 2 complement
 # https://www.cs.cornell.edu/~tomf/notes/cps104/twoscomp.html
 #
 # https://en.wikipedia.org/wiki/Two%27s_complement
 #
+# TODO
 def twos_complement (input_value, num_bits):
     mask = 2**(num_bits - 1)
     return -(input_value & mask) + (input_value & ~mask)
+
+# TODO
+def two_complement_b(e):
+  for e in pulseOut:
+    if e < 0:
+        s = 1
+    else:
+        s= 0
+    a = str(s) + str(bin(abs(e))[2:].zfill(14))
+    print a
+
 
 pulse = []
 RINIT = 1028
@@ -49,13 +63,5 @@ for i,e in enumerate(pulseOut):
     pulseOut[i] = int(pulseOut[i] * RADC / VADC + random.randint(-30,30))
 
 for e in pulseOut:
-    if e < 0:
-        s = 1
-    else:
-        s= 0
-    a = str(s) + str(bin(abs(e))[2:].zfill(14))
-    print a
-
-for e in pulseOut:
-    a = twos_complement(e, 14)
-    print a
+    a = np.binary_repr(e, 16)
+    print a, e
