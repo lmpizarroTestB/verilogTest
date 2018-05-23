78191924c4fd0273f0281a3629c9494d73b762ae
Luis María Pizarro <lmpizarro@gmail.com>
2018-05-23 13:38:52 -0300

    verious modules

--- src/filters/various_module.v
+++ src/filters/various_module.v
@@ -37,17 +37,19 @@ module Shift_Register #(parameter Nbits = 16)
 endmodule
 
 module Discriminator #(parameter Nbits = 16)
-       (DUL, LL, clk, xn, yn, mode);
+       (DUL, LL, clk, xn, yn, mode, Q);
 
   input clk, mode;
   input [Nbits-1:0] DUL;
   input [Nbits-1:0] LL;
   input [Nbits-1:0] xn;
+  output Q;
   output [Nbits-1:0] yn;
 
   reg [Nbits-1:0] tmp;
   reg [Nbits-1:0] UL;
   reg [Nbits-1:0] MAX_ = 1<<Nbits;
+  reg Q_;
 
   always @(posedge clk)
     begin
@@ -55,16 +57,29 @@ module Discriminator #(parameter Nbits = 16)
         UL = DUL + LL;
         if (UL < LL) UL = MAX_;
         if (xn > LL && xn < UL)
+        begin
           tmp = xn;
+	  Q_ = 1;
+        end  
         else
+        begin
 	  tmp = 0;
+	  Q_ = 0;
+        end  
       end
       else begin
         if (xn > LL)
+	begin
           tmp = xn;
+	  Q_ = 1;
+	end
         else
+	begin
 	  tmp = 0;
+	  Q_ = 0;
+        end
       end	      
     end
-    assign Q = tmp;
+    assign yn = tmp;
+    assign Q = Q_;
 endmodule
