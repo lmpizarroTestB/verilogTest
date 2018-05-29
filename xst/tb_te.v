504e7814c103575cd2690e24b787732b61b28335
Luis María Pizarro <lmpizarro@gmail.com>
2018-05-29 16:53:03 -0300

    refactoring rx

--- xst/tb_te.v
+++ xst/tb_te.v
@@ -2,72 +2,88 @@
 `include "three_st.v"
 `timescale 1ns/1ps
 
-module ABC (clk, in, out);
-input clk, in;
-output out;
+module ABC (clk, rx, out);
+ input clk, rx;
+ output out;
 
-reg [3:0] cnt10 = 4'b0000;
-reg [4:0] cntbit = 4'b0000;
-reg [7:0] cntFrame = 8'b00000000;
-reg cd = 0; 
+ reg [3:0] cnt10 = 4'b0000;
+ reg [4:0] cntbit = 5'b00000;
+ reg [7:0] cntFrame = 8'b00000000;
 
-reg outl = 1'b0;
-reg outp = 1'b0;
-reg ffbb = 1'b0;
-reg bitPulse = 1'b0;
+ reg delayBitStart = 1'b0;
+ reg centerStartBitPulse = 1'b0;
+ reg validFrame = 1'b0;
+ reg clkBit = 1'b0;
+ reg cd = 0; 
 
-
-always @(negedge (in|ffbb))
+ always @(negedge (rx|validFrame))
  begin
-   outl <= 1'b1;
+  delayBitStart <= 1'b1;
  end
 
-always @(posedge clk)
-begin
- if (outl == 1'b1)
+ always @(negedge delayBitStart)
  begin
-   cnt10 <= cnt10 + 1;
-   if (cnt10 == 4'b1001) 
-   begin
-    outl <= 1'b0;
-    cnt10 <= 4'b0000;
-    ffbb <= 1'b1;
+   centerStartBitPulse = 1'b1 & ~rx;
+   cd = 1'b1;
+   cntbit = 5'b00000;
+ end
+
+ always @(posedge clk)
+ begin
+  if (delayBitStart == 1'b1)
+  begin
+    cnt10 <= cnt10 + 1;
+    if (cnt10 == 4'b1001) 
+    begin
+     delayBitStart <= 1'b0;
+     cnt10 <= 4'b0000;
+    end
+  end
+ end
+
+ always @(posedge clk)
+ begin
+  if (cd==1'b1) begin
+   centerStartBitPulse = 1'b0;
+   cd = 1'b0;
+  end
+ end
+
+ always @(negedge centerStartBitPulse)
+ begin
+   validFrame <= 1'b1;
+ end
+
+
+ // generate clkBit
+ always @(posedge (clk))
+ begin
+  if ( validFrame) begin
+   cntbit = cntbit + 5'b00001;
+   if (cntbit == 5'b10100) 
+   begin 
+    cntbit <= 5'b00000;
+    clkBit <= 1'b1;
    end
- end
- if (cd==1'b1) begin
-  outp <= 1'b0;
-  cd <= 1'b0;
- end
-end
-
-always @(negedge outl)
-begin
-  outp <= 1'b1 & ~in;
-  cd <= 1'b1;
-end
-
-always @(posedge clk&ffbb)
-begin
-  cntFrame = cntFrame + 1;
-  if (cntFrame == 8'b11010011) begin 
-	  cntFrame <= 8'b00000000;
-	  ffbb = 1'b0;
+   else clkBit <= 1'b0;
   end
-end
+ end
 
-
-always @(posedge (clk&ffbb))
-begin
-  cntbit = cntbit + 1;
-  if (cntbit == 5'b10100) begin 
-	  cntbit <= 5'b00000;
-	  bitPulse <= 1'b1;
+ // generate  frame valid
+ always @(posedge (clk))
+ begin
+  if ( validFrame) begin
+   cntFrame = cntFrame + 1;
+   if (cntFrame == 8'b11010011) 
+   begin 
+    cntFrame <= 8'b00000000;
+    validFrame = 1'b0;
+   end
   end
-  else bitPulse <= 1'b0;
-end
+ end
 
+ assign out = centerStartBitPulse;
 
-assign out = outp;
 endmodule
 
 /*---------------------------------------------
@@ -104,10 +120,10 @@ ABC DUT1 (clk, rx, out);
       for (i =0; i < 500; i = i + 1)
          #10 in = 0;
       #10 in = 0; rx=0;
-      for (i =0; i < 40; i = i + 1)
+      for (i =0; i < 10; i = i + 1)
          #10 in = 0;
       #10 in = 0; rx=1;
-      for (i =0; i < 40; i = i + 1)
+      for (i =0; i < 100; i = i + 1)
          #10 in = 0;
       $finish;
     end
