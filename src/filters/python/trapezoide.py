7fb88c4dba3246eb398908c31aa6c4bc9037907b
lmpizarro <lmpizarro@gmail.com>
2018-06-29 20:13:36 -0300

    trape opt

--- src/filters/python/trapezoide.py
+++ src/filters/python/trapezoide.py
@@ -12,75 +12,9 @@ except ImportError:
 '''
 ?????
 '''
-def gaussian (s):
-
-    sd2 = np.append(s[2:],np.zeros(2))
-    sd1 = np.append(s[1:],np.zeros(1))
-    y = s + sd2 + sd1*2 
-    plt.plot(sd2[10:40])
-    plt.plot(sd1[10:40])
-    plt.plot(y[10:40])
-    plt.show()
-
-
-
-    return y
-
-def test():
-  N = 500
-  d=10
-  c = .2
-
-  s1 = np.ones(N-d)
-  v = np.zeros(N)
-
-  v[d:] = s1 * 3000
-
-  for i in range(1,v.shape[0]):
-      v[i] = v[i] - 4.0*i
-
-  d = c*v
-
-  delay = 5
-
-  d_delay = np.zeros(d.shape[0]+delay)
-  d_delay[delay:] = d
-  v_delay = np.zeros(d.shape[0]+delay)
-  v_delay[delay:] = d
-
-  d = np.append(d, np.zeros(delay))
-  v = np.append(d, np.zeros(delay))
-
-  k = np.asarray([i for i in range(d.shape[0])]) 
-  diff = d - d_delay
-
-  Dn=np.zeros(diff.shape[0])
-  un=np.zeros(diff.shape[0])
-  sn=np.zeros(diff.shape[0])
-  
-  for i in range (1,Dn.shape[0]):
-      Dn[i]=Dn[i-1] + diff[i]
-      un[i] = v[i] + Dn[i-1]
-
-
-  for i in range (1,Dn.shape[0]):
-      sn[i] = un[i] - v_delay[i]
-
-  try:
-   plt.plot(Dn)
-   plt.plot(v, '.', color='black')
-   plt.show()
-  except NameError:
-      pass
-
-def main(signal, paddi, delayL):
-  delayLS =[0.0]*(paddi+delayL) 
-  delayLS.extend(signal)
-  diff_len = len(delayLS) - len(signal)
-  signal.extend([0.0]*diff_len)
-  dsL = np.asarray(signal) - np.asarray(delayLS)
-  print dsL[:-50].shape[0]
-  return dsL
+Ts = 1/100E6
+Tao = 1E-6
+plot = False
 
 
 def modDS(signal, delayL):
@@ -89,15 +23,15 @@ def modDS(signal, delayL):
   delayLS = np.append(del_, signal)
   signal = np.append(signal, del_)
 
-  dsL = signal - delayLS
+  dsL = np.floor(signal - delayLS)
   return dsL[:-delayL]
 
-def HPD(dsk, m1, m2):
+def HPD(dsK, m1, m2):
   acc1 = np.zeros(dsK.shape[0])
   mult = np.zeros(dsK.shape[0])
 
   for i in range(1, dsK.shape[0]):
-    acc1[i] = acc1[i-1] + .125*dsK[i]/4
+    acc1[i] = acc1[i-1] + m2*dsK[i]/4
   acc1 = np.floor(acc1)
 
   for i in range(dsK.shape[0]):
@@ -109,11 +43,11 @@ def HPD(dsk, m1, m2):
   sn = np.zeros(rn.shape[0])
 
   for i in range(1, sn.shape[0]):
-      sn[i] = sn[i-1] + m2*rn[i]
+      sn[i] = sn[i-1] + rn[i]
   sn = np.floor(sn)
   return rn, sn
 
-def Signal(N, padd, Ts, Tao, amp=1000):
+def Signal( Ts, Tao, amp=1000, padd = 20,N=500 ):
 
     t= np.linspace(-padd*Ts, N*Ts, num=N+padd)
 
@@ -135,13 +69,6 @@ def calcM(Tao=50E-6, Tclk=1E-8, m2=1):
     M= 1 / (np.exp(Tclk/Tao) - 1)
     m1 = M * m2
     return M, m1
-'''
-Tao    M            m1       m2
-5E-6 499.500167 14.985005 0.030000 -> noise .5
-3E-6 299.500278 8.985008 0.030000 -> noise .875
-10E-6 999.500083 29.985002 0.030000 -> noise 0.25
-50E-6 4999.500017 249.975001 0.050000 
-'''
 
 def poleZero(s,Tao, Ts):
     I=np.zeros(s.shape[0])
@@ -152,62 +79,95 @@ def poleZero(s,Tao, Ts):
 
     return s +I
 
-def noise(s,u):
+def noise(s,u, seed=3):
     #n = np.floor(np.random.normal(0, u, s.shape[0]))
+    np.random.seed(seed)
     n = np.floor(np.random.random_integers(-u, u, s.shape[0]))
     return s + n
 
-if __name__ == "__main__":
-  paddi = 20
-  N = 500
-  amp = 16000
-  plot = False
 
-  delayL = 15
-  delayK = 30
-  Ts = 1/100E6
-  Tao = 3E-6
-
-  print Tao/Ts, 2**23
-  m2 =  .82 
-
-  M,m1 = calcM(Tao = Tao, Tclk = Ts, m2=m2)
-  print "%f %f %f"%(M, m1, m2)
-
-
-  amps =[160, 320, 640, 1280, 1600, 2560, 5120, 8000, 10240, 16000]
-  #noises = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,20]
-  noises = [20]
-  max_ss = {} 
-  for noi in noises:
-     max_s = []
-     sum_max = 0
-     for amp in amps:
-       t,s = Signal(N, paddi, Ts, Tao, amp=amp)
-
-       snoise = noise(s,noi)
-       spz = poleZero(snoise, Tao, Ts)
-
+def trapez(spz, m1, m2, delayL=15, delayK=30 ):
        dsL = modDS (spz, delayL)
        dsK = modDS(dsL, delayK)
 
        rn,sn = HPD(dsK, m1, m2)
-       max_ = PHA(rn)
-       pc = int(10000.0 * (max_ - amp)/amp)/100.0
-       max_s.append(pc)
-       pc = pc*pc
-       sum_max = sum_max + pc
-     print m2, np.sqrt(sum_max)
-     max_ss[str(noi)] = (max_s)
+       return rn, sn
 
-  for noi in noises:
-      print noi, max_ss[str(noi)]
+def calcAmps(amps, m2, Tao):
+  max_s = []
+  sum_max = 0
 
-  if (plot):
-   R = 100
-   try:
-    plt.plot(t[:R],sn[:R])
-    plt.plot(t[:R],sn[:R])
-    plt.show()
-   except NameError:
+  noi= 0
+
+
+  M,m1 = calcM(Tao = Tao, Tclk = Ts, m2=m2)
+
+
+  for am in amps:
+
+    t,s = Signal(Ts, Tao, amp=am)
+    snoise = noise(s,noi)
+
+    spz = poleZero(snoise, Tao, Ts)
+    rn,sn = trapez(spz, m1, m2)
+
+    max_ = PHA(rn)
+    max_s.append(max_)
+
+  return m1, np.asarray(max_s)
+
+
+def calcDevs(max_s, amps):
+    if (plot):
+      R = 100
+    try:
+      plt.plot(t[:R],sn[:R])
+      plt.plot(t[:R],sn[:R])
+      plt.show()
+    except NameError:
       pass
+
+    pc = np.abs(np.floor(10000.0 * (max_s - amps)/amps)/100.0)
+
+    return  pc.sum()/amps.shape[0]
+
+
+'''
+Tao   m1       m2
+1E-6  255.0363  2.5631 
+3E-6  250.6 0.836 
+5E-6  262.894824561 0.52631578947
+10E-6 241.984230702 0.242105263158
+50E-6 246.028027136 0.0492105 
+'''
+def main1():
+  amps =np.asarray([160, 320, 640, 1280, 1600, 2560, 5120, 8000, 10240, 16000])
+  #amps = np.random.random_integers(160, 160*100, 10)
+  #noises = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,20]
+
+  Tao =50E-6
+  for m2 in np.linspace(.04, 0.06, 20):
+    m1, max_s = calcAmps(amps, m2, Tao)
+    devs = calcDevs(amps, max_s)
+    print m1, m2, devs 
+
+def main50():
+    amp =8000
+    Tao =50E-6
+    Ts = 1E-8
+    m1 = 256
+    m2 = 1/32.0#0.0515
+    noi = 0
+
+    t,s = Signal(Ts, Tao, amp=amp)
+    snoise = noise(s,noi)
+
+    spz = poleZero(snoise, Tao, Ts)
+    rn,sn = trapez(spz, m1, m2)
+    max_ = PHA(rn)
+
+    print rn[0:100]
+
+    print max_
+if __name__ == "__main__":
+    main50()
